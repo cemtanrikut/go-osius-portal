fbf95ad212d0e9e8def517431e24e356b7af3fbb
Cem Tanrikut <cemtanrikut@gmail.com>
2025-02-25 18:07:30 +0100

    added run server

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/config"
+	"main.go/routes"
+)
+
+func main() {
+	config.ConnectDatabase()
+
+	r := gin.Default()
+	routes.SetupRoutes(r)
+
+	r.Run(":8080")
+}
