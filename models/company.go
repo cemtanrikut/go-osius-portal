29d19110e1709f566336a9a7e2f968804df3484c
Cem Tanrikut <cemtanrikut@gmail.com>
2025-02-25 17:02:09 +0100

    added company model

---
+++ models/company.go
@@ -0,0 +1,28 @@
+package models
+
+type Company struct {
+	ID          string      `bson:"_id,omitempty" json:"id"`
+	Name        string      `bson:"name" json:"name"`
+	Phone       string      `bson:"phone" json:"phone"`
+	Email       string      `bson:"email" json:"email"`
+	Tags        string      `bson:"tags" json:"tags"`
+	Status      bool        `bson:"status" json:"status"`
+	CreatedAt   string      `bson:"created_at" json:"created_at"`
+	CompanyInfo CompanyInfo `bson:"company_info" json:"company_info"`
+}
+
+type CompanyInfo struct {
+	CompanyName string `bson:"company_name" json:"company_name"`
+	Address     string `bson:"address" json:"address"`
+	Postcode    string `bson:"postcode" json:"postcode"`
+	Country     string `bson:"country" json:"country"`
+	KVK         string `bson:"kvk" json:"kvk"`
+	BTWnumber   string `bson:"btw" json:"btw"`
+	LastUpdate  string `bson:"last_update" json:"last_update"`
+}
+
+type CompanyRepresentative struct {
+	Name  string `bson:"name" json:"name"`
+	Email string `bson:"email" json:"email"`
+	Phone string `bson:"phone" json:"phone"`
+}
