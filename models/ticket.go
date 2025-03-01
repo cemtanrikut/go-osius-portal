a8014303aecd0be7d0eb1b4b2b06e1f11f9aaaa9
Cem Tanrikut <cemtanrikut@gmail.com>
2025-03-01 21:29:56 +0100

    added swagger library

--- models/ticket.go
+++ models/ticket.go
@@ -1,15 +1,18 @@
 package models
 
 type Ticket struct {
-	ID          uint   `gorm:"primaryKey"`
-	Title       string `bson:"title" json:"title"`
-	Company     string `bson:"company" json:"company"`
-	Location    string `bson:"location" json:"location"`
-	ReferanceNo string `bson:"referance_no" json:"referance_no"`
-	Person      string `bson:"person" json:"person"`
-	TicketType  string `bson:"ticket_type" json:"ticket_type"`
-	Description string `bson:"description" json:"description"`
-	Files       string `bson:"files" json:"files"`
-	Status      string `bson:"status" json:"status"`
-	CreateDate  string `bson:"create_date" json:"create_date"`
+	ID            uint   `gorm:"primaryKey"`
+	Title         string `bson:"title" json:"title"`
+	Company       string `bson:"company" json:"company"`
+	Location      string `bson:"location" json:"location"`
+	ReferanceNo   string `bson:"referance_no" json:"referance_no"`
+	Person        string `bson:"person" json:"person"`
+	TicketType    string `bson:"ticket_type" json:"ticket_type"`
+	Description   string `bson:"description" json:"description"`
+	Files         string `bson:"files" json:"files"`
+	Status        string `bson:"status" json:"status"`
+	CreatedDate   string `bson:"create_date" json:"create_date"`
+	UpdatedDate   string `bson:"update_date" json:"update_date"`
+	CompletedDate string `bson:"completed_date" json:"completed_date"`
+	IsActive      bool   `bson:"is_active" json:"is_active"`
 }
